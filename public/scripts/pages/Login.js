2b04ef765c7f175a81361379fdf5e4ff4cf430f7
Bu Kinoshita <bukinoshita@gmail.com>
2016-09-30 14:36:09 -0300

    Login & Sign Up

--- public/scripts/pages/Login.js
+++ public/scripts/pages/Login.js
@@ -1,8 +1,38 @@
 'use strict'
 
 import React, {Component} from 'react'
+import {browserHistory} from 'react-router'
 
 export default class Login extends Component {
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.submitLogin = this.submitLogin.bind(this)
+
+    this.state = {email: '', password: ''}
+  }
+
+  handleChange (e) {
+    this.setState({[e.target.name]: e.target.value})
+
+    console.log(this.state)
+  }
+
+  submitLogin (e) {
+    e.preventDefault()
+
+    return fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password
+      })
+    }).then((res) => {
+      browserHistory.push('/profile')
+    })
+  }
+
   render () {
     return (
       <div>
@@ -10,15 +40,15 @@ export default class Login extends Component {
           <h2 className='title'>Login</h2>
           <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel lacus vel augue aliquet luctus. Ut urna libero.</p>
 
-          <form className='registration-form'>
+          <form className='registration-form' onSubmit={this.submitLogin}>
             <fieldset className='form-input'>
               <label className='label'>E-mail</label>
-              <input className='input' type='text' name='email' />
+              <input className='input' type='text' name='email' onChange={this.handleChange} />
             </fieldset>
 
             <fieldset className='form-input'>
               <label className='label'>Password</label>
-              <input className='input' type='password' name='password' />
+              <input className='input' type='password' name='password' onChange={this.handleChange} />
             </fieldset>
 
             <button className='btn -secondary -large' type='submit'>Login</button>
--- public/scripts/pages/SignUp.js
+++ public/scripts/pages/SignUp.js
@@ -3,6 +3,34 @@
 import React, {Component} from 'react'
 
 export default class SignUp extends Component {
+  constructor (props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.submitSignup = this.submitSignup.bind(this)
+
+    this.state = {name: '', email: '', password: ''}
+  }
+
+  handleChange (e) {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  submitSignup (e) {
+    e.preventDefault()
+
+    return fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password
+      })
+    }).then((res) => {
+      console.log(res)
+    })
+  }
+
   render () {
     return (
       <div>
@@ -10,18 +38,23 @@ export default class SignUp extends Component {
           <h2 className='title'>Sign Up</h2>
           <p className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel lacus vel augue aliquet luctus. Ut urna libero.</p>
 
-          <form className='registration-form' action='/signup' method='post'>
+          <form className='registration-form' onSubmit={this.submitSignup}>
+            <fieldset className='form-input'>
+              <label className='label'>Name</label>
+              <input className='input' type='text' name="name" onChange={this.handleChange} />
+            </fieldset>
+
             <fieldset className='form-input'>
               <label className='label'>E-mail</label>
-              <input className='input' type='text' name='email' />
+              <input className='input' type='text' name="email" onChange={this.handleChange} />
             </fieldset>
 
             <fieldset className='form-input'>
               <label className='label'>Password</label>
-              <input className='input' type='password' name='password' />
+              <input className='input' type='password' name="password" onChange={this.handleChange} />
             </fieldset>
 
-            <button className='btn -secondary -large' type='submit'>Login</button>
+            <button className='btn -secondary -large' type='submit'>Sign Up</button>
           </form>
         </div>
       </div>
