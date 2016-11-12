1467b5aef2d24e4ab32209fcc32db598139e0e4c
Bu Kinoshita <bukinoshita@gmail.com>
2016-11-12 16:49:05 -0500

    Reducer & Action test

--- __tests__/open-modal.js
+++ __tests__/open-modal.js
@@ -5,7 +5,7 @@ import test from 'ava'
 import openModalReducer from '../reducers/open-modal'
 import openModalAction from '../actions/open-modal'
 
-test('open modal', t => {
+test('reducer modal', t => {
   const state = [{name: 'Add Summoners', open: false}, {name: 'Add Location', open: false}]
   const index = 0
   const action = {type: 'OPEN_MODAL', index}
@@ -14,7 +14,7 @@ test('open modal', t => {
   t.is(data[index].open, true)
 })
 
-test('open summoners modal', t => {
+test('action modal', t => {
   const index = 1
   const expectedAction = {type: 'OPEN_MODAL', index}
 
