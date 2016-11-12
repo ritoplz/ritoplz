5ceede790fe76e3ca664e72a435036faa46b2481
Bu Kinoshita <bukinoshita@gmail.com>
2016-11-12 16:17:05 -0500

    Single Quotes

--- __tests__/open-modal.js
+++ __tests__/open-modal.js
@@ -5,7 +5,7 @@ import test from 'ava'
 import openModal from '../reducers/open-modal'
 
 test('open modal', t => {
-  const state = [{name: "Add Summoners", open: false}, {name: "Add Location", open: false}]
+  const state = [{name: 'Add Summoners', open: false}, {name: 'Add Location', open: false}]
   const index = 0
   const action = {type: 'OPEN_SUMMONERS_MODAL', index}
   const data = openModal(state, action)
