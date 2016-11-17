a0bb2b5587bc9afb3b72100ddf9a8418cf7543c8
Bu Kinoshita <bukinoshita@gmail.com>
2016-11-17 12:11:09 -0500

    Routing PropTypes is Required

--- pages/signup.js
+++ pages/signup.js
@@ -6,6 +6,10 @@ import { Provider } from 'react-redux'
 import configureStore from '../store/configureStore'
 import FormSignUp from '../containers/form-sign-up'
 
+const propTypes = {
+  routing: React.PropTypes.object.isRequired
+}
+
 const SignUp = props => {
   const store = configureStore()
 
