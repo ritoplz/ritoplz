8309b9cd61f0e2f6867c3696dcb99533529bd1be
Bu Kinoshita <bukinoshita@gmail.com>
2016-11-17 12:36:40 -0500

    Change Form Sign Up to Class to Get Refs

--- containers/form-sign-up.js
+++ containers/form-sign-up.js
@@ -5,32 +5,59 @@ import { connect } from 'react-redux'
 
 import signUpRequest from '../actions/sign-up'
 
-const FormSignUp = () => {
-  const handleSignup = (e) => {
-    e.preventDefault()
+const propTypes = {
+  signUpRequest: React.PropTypes.func.isRequired,
+  routing: React.PropTypes.object.isRequired
+}
+
+class FormSignUp extends Component {
+  constructor () {
+    super()
+
+    this.handleSignup = this.handleSignup.bind(this)
   }
 
-  return (
-    <form className="registration-form">
-      <fieldset className="form-input">
-        <label className="label">Name</label>
-        <input className="input" type="text"/>
-      </fieldset>
+  handleSignup(e) {
+    e.preventDefault()
 
-      <fieldset className="form-input">
-        <label className="label">E-mail</label>
-        <input className="input" type="text"/>
-      </fieldset>
+    const data = {
+      name: this.name.value,
+      email: this.email.value,
+      password: this.password.value
+    }
 
-      <fieldset className="form-input">
-        <label className="label">Password</label>
-        <input className="input" type="password"/>
-      </fieldset>
+    console.log(data)
 
-      <button className="btn -secondary -large" type="submit">Sign Up</button>
-    </form>
-  )
+    this.props.signUpRequest(data).then(() => {
+      this.props.routing.url.pushTo('/login')
+    })
+  }
+
+  render () {
+    return (
+      <form className="registration-form" onSubmit={this.handleSignup}>
+        <fieldset className="form-input">
+          <label className="label">Name</label>
+          <input className="input" type="text" ref={input => this.name = input}/>
+        </fieldset>
+
+        <fieldset className="form-input">
+          <label className="label">E-mail</label>
+          <input className="input" type="email" ref={input => this.email = input}/>
+        </fieldset>
+
+        <fieldset className="form-input">
+          <label className="label">Password</label>
+          <input className="input" type="password" ref={input => this.password = input}/>
+        </fieldset>
+
+        <button className="btn -secondary -large" type="submit">Sign Up</button>
+      </form>
+    )
+  }
 }
+
+FormSignUp.propTypes = propTypes
 
 const mapStateToProps = (state, ownProps) => {
   return {
