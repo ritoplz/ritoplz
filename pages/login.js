beabdcc5ece7de19c4d72ef2767381de5e446a73
Bu Kinoshita <bukinoshita@gmail.com>
2016-11-17 12:01:03 -0500

    Login Provide Store

--- pages/login.js
+++ pages/login.js
@@ -3,17 +3,23 @@
 /* global localStorage: false */
 
 import React from 'react'
+import { Provider } from 'react-redux'
 
+import configureStore from '../store/configureStore'
 import FormLogin from '../containers/form-login'
 
 const Login = () => {
-  return (
-    <div className="row">
-      <h2 className="title">Login</h2>
-      <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel lacus vel augue aliquet luctus. Ut urna libero.</p>
+  const store = configureStore()
 
-      <FormLogin />
-    </div>
+  return (
+    <Provider store={store}>
+      <div className="row">
+        <h2 className="title">Login</h2>
+        <p className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel lacus vel augue aliquet luctus. Ut urna libero.</p>
+
+        <FormLogin />
+      </div>
+    </Provider>
   )
 }
 
