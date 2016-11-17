d5ccd1a85871d62c29cbdbfaeb7b830783e72912
Bu Kinoshita <bukinoshita@gmail.com>
2016-11-17 11:59:48 -0500

    Login Page

--- pages/login.js
+++ pages/login.js
@@ -4,11 +4,9 @@
 
 import React from 'react'
 
+import FormLogin from '../containers/form-login'
+
 const Login = () => {
-  const handleLogin () {
-
-  }
-
   return (
     <div className="row">
       <h2 className="title">Login</h2>
@@ -19,46 +17,4 @@ const Login = () => {
   )
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-export default class Login extends Component {
-  constructor (props) {
-    super(props)
-    this.handleLogin = this.handleLogin.bind(this)
-  }
-
-  handleLogin (e) {
-    e.preventDefault()
-
-    axios.post('http://localhost:3001/login', {
-      email: this.email.value,
-      password: this.password.value
-    }).then(res => {
-      const token = res.data.token
-      localStorage.setItem('token', token)
-      this.props.url.pushTo('/profile')
-    }).catch(err => {
-      console.log(err)
-    })
-  }
-
-  render () {
-    return (
-      
-    )
-  }
-}
-
-Login.propTypes = {
-  url: React.PropTypes.object
-}
+export default Login
