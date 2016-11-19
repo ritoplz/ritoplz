448c757dc1057063fb158c260e4f86d0fa63da99
Bu Kinoshita <bukinoshita@gmail.com>
2016-11-18 21:24:02 -0500

    Add Sign Up ActionTypes & Refactoring Sign Up Action

--- actions/sign-up.js
+++ actions/sign-up.js
@@ -2,10 +2,33 @@
 
 import axios from 'axios'
 
-function signUpRequest (userData) {
-  return dispatch => {
-    return axios.post('http://localhost:3001/signup', userData)
+import * as types from './constants'
+
+function signupRequest () {
+  return {type: types.SIGNUP_REQUEST}
+}
+
+function signupSuccess (data) {
+  return {
+    type: types.SIGNUP_SUCCESS,
+    data
   }
 }
 
-export default signUpRequest
+function signupError (data) {
+  return {
+    type: types.SIGNUP_ERROR,
+    data
+  }
+}
+
+function handleSignup (userData) {
+  return dispatch => {
+    dispatch(signupRequest())
+    return axios.post('http://localhost:3001/signup', userData)
+      .then(res => dispatch(signupSuccess(res.data)))
+      .catch(res => dispatch(signupError(res.data)))
+  }
+}
+
+export default handleSignup
---
+++ constants/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+export const SIGNUP_REQUEST = 'SIGNUP_REQUEST'
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS'
+export const SIGNUP_ERROR = 'SIGNUP_ERROR'
