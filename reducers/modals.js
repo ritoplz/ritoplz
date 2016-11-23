ad96e3eaab9b42a203c17a6057b6811cf613c4a1
Bu Kinoshita <bukinoshita@gmail.com>
2016-11-22 22:24:35 -0500

    Update

--- reducers/modals.js
+++ reducers/modals.js
@@ -10,11 +10,6 @@ const initialState = {
 export default (state = initialState, action) => {
   switch(action.type) {
     case types.OPEN_MODAL:
-      const nextState = if (state.hasOwnProperty(action.modal)) {
-        return state
-      }
-      console.log(nextState)
-
       return 'nice'
 
     default:
