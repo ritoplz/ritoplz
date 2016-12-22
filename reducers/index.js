36c339fa9477ac111636e90b31e702358889bdb1
Bu Kinoshita <bukinoshita@gmail.com>
2016-12-22 18:24:40 -0500

    Combine Reducers

--- reducers/index.js
+++ reducers/index.js
@@ -6,10 +6,12 @@ import signUp from './sign-up'
 import login from './login'
 import user from './user'
 import addSummoner from './add-summoner'
+import rankings from './rankings'
 
 export default combineReducers({
   signUp,
   login,
   user,
-  addSummoner
+  addSummoner,
+  rankings
 })
