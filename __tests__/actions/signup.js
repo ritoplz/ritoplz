ab428ed1b2ddd47e53a28bc961875c5dd6b4e580
Bu Kinoshita <bukinoshita@gmail.com>
2016-12-27 18:24:33 -0500

    Test

--- __tests__/actions/signup.js
+++ __tests__/actions/signup.js
@@ -15,23 +15,19 @@ const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
 
 describe('signup action', () => {
-  afterEach(() => {
-    nock.clearAll()
-  })
-
   it('should dispatch SIGNUP_SUCCESS when requesting signup', () => {
     nock('http://localhost:3001/')
       .post('/signup')
-      .reply(200, { body: { todos: {} }})
+      .reply(200, { body: { data: {} }})
 
     const expectedActions = [
       { type: SIGNUP_REQUEST },
-      { type: SIGNUP_SUCCESS, body: { todos: {} }}
+      { type: SIGNUP_SUCCESS, body: { data: {} }}
     ]
 
     const initialState = {}
 
     const store = mockStore(initialState)
-    console.log(store.getActions())
+    expect(1).toEqual(1)
   })
 })
