d13fc730d2412c6cfdc19446a8c3bcb3d3a71d03
Bu Kinoshita <bukinoshita@gmail.com>
2016-12-30 14:12:08 -0500

    Footer UI

--- components/footer.js
+++ components/footer.js
@@ -5,45 +5,86 @@ import Link from 'next/link'
 import { style } from 'next/css'
 
 const styles = {
+  footer: {
+    paddingTop: '20px',
+    paddingBottom: '20px'
+  },
+
   row: {
     maxWidth: '900px',
     marginLeft: 'auto',
-    marginRight: 'auto'
+    marginRight: 'auto',
+    display: 'flex',
+    justifyContent: 'space-between'
   },
 
-  footer: {
-    height: '120px',
-    background: 'linear-gradient(to right, #52bdab 0%,#6BB6D6 100%)',
+  copyright: {
+    color: '#666',
+    fontSize: '14px'
+  },
+
+  item: {
+    display: 'inline-block',
+    borderRight: '1px solid #ededed',
+    verticalAlign: 'middle',
+    marginRight: '15px',
+    height: '20px',
+    lineHeight: '20px',
+    paddingRight: '15px'
+  },
+
+  link: {
+    color: '#666',
+    textDecoration: 'none',
+    fontSize: '12px',
+    transition: 'all .2s ease',
+    textDecoration: 'none',
+
+    ':hover': {
+      color: '#ccc'
+    }
   }
 }
 
 export default props => {
   return (
-    <footer>
-      <div className="row">
-        <Link href="/">Ritoplz</Link>
+    <footer className={style(styles.footer)}>
+      <div className={style(styles.row)}>
+        <span className={style(styles.copyright)}>Copyright © 2016 Ritoplz. All rights reserved.</span>
 
-        <ul>
-          <li>
-            <Link href="/faq">FAQ</Link>
-          </li>
+        <nav className={style(styles.footerLinks)}>
+          <ul>
+            <li className={style(styles.item)}>
+              <Link href="">
+                <img src="/static/github.svg"/>
+              </Link>
+            </li>
 
-          <li>
-            <Link href="/terms">Terms</Link>
-          </li>
+            <li className={style(styles.item)}>
+              <Link href="">
+                <img src="/static/twitter.svg"/>
+              </Link>
+            </li>
 
-          <li>
-            <Link href="https://twitter.com">Twitter</Link>
-          </li>
+            <li className={style(styles.item)}>
+              <Link href="">
+                <span className={style(styles.link)}>Terms of Service</span>
+              </Link>
+            </li>
 
-          <li>
-            <Link href="https//github.com">Github</Link>
-          </li>
+            <li className={style(styles.item)}>
+              <Link href="">
+                <span className={style(styles.link)}>FAQ</span>
+              </Link>
+            </li>
 
-          <li>
-            <Link href="mailto:ritoplz@gmail.com">Contact</Link>
-          </li>
-        </ul>
+            <li className={style(styles.item)}>
+              <Link href="">
+                <span className={style(styles.link)}>ritoplz@gmail.com</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </footer>
   )
