66e4f118a57981c2401972586e481309cb4cca74
Bu Kinoshita <bukinoshita@gmail.com>
2017-01-02 14:05:32 -0500

    Redux Logger

--- store/configureStore.js
+++ store/configureStore.js
@@ -2,15 +2,18 @@
 
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
 
 import rootReducer from '../reducers'
+
+const logger = createLogger()
 
 const configureStore = initialState => {
   return createStore(
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(thunk),
+      applyMiddleware(thunk, logger),
       typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
       ? window.devToolsExtension()
       : f => f
