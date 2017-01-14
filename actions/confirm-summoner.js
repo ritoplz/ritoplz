e0b656babf80f8a56093be012610909d62a3701f
Bu Kinoshita <bukinoshita@gmail.com>
2017-01-13 21:43:10 -0500

    API Url

--- actions/confirm-summoner.js
+++ actions/confirm-summoner.js
@@ -37,7 +37,7 @@ function handleConfirmSummoner (summoner) {
     dispatch(confirmSummonerRequest())
     return axios({
       method: 'post',
-      url: 'http://localhost:3001/summoner/confirm',
+      url: 'https://staging.ritoplz.com/summoner/confirm',
       data,
       headers: {
         'Content-Type': 'application/json',
