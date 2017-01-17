3dbca9ad26acff7194e50b555c0174cb086828e5
Bu Kinoshita <bukinoshita@gmail.com>
2017-01-17 13:52:18 -0500

    Add analytics

--- components/meta.js
+++ components/meta.js
@@ -33,6 +33,7 @@ export default () => (
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
       <script src="static/drift.js"></script>
+      <script src="static/analytics.js"></script>
 
       <link rel="apple-touch-icon" sizes="57x57" href="static/apple-icon-57x57.png"/>
       <link rel="apple-touch-icon" sizes="60x60" href="static/apple-icon-60x60.png"/>
