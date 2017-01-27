bf3c3ffe9e06230703428bd40c24c1fb1dce8b92
Bu Kinoshita <bukinoshita@gmail.com>
2017-01-26 19:49:12 -0500

    Update api.js

--- services/api.js
+++ services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getToken, isLogged } from '../services/auth'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://staging.ritoplz.com',
   headers: {
     Accept: 'application/json'
   }
