3d630032872510c66dfaf9c7ac3bb1205607c5b1
Bu Kinoshita <bukinoshita@gmail.com>
2017-05-01 15:21:55 -0400

    Upgrade onboard

--- containers/onboard.js
+++ containers/onboard.js
@@ -23,180 +23,6 @@ import {
   ACCOUNT_ERROR
 } from './../constants'
 
-const styles = {
-  formInput: {
-    border: 'none',
-    marginBottom: '20px'
-  },
-
-  label: {
-    display: 'inline-block',
-    marginBottom: '10px',
-    fontWeight: 600,
-    fontSize: '1.15rem',
-    color: '#333'
-  },
-
-  input: {
-    padding: '20px 15px',
-    width: '100%',
-    border: '1px solid #eee',
-    borderRadius: '5px',
-    fontSize: '1.1rem',
-    outline: 'none',
-
-    ':focus': {
-      borderColor: '#ccc'
-    }
-  },
-
-  title: {
-    textAlign: 'center',
-    color: '#333',
-    fontSize: '2rem',
-    fontWeight: '500',
-    marginBottom: '10px'
-  },
-
-  subtitle: {
-    color: '#999',
-    fontSize: '1.15rem',
-    fontWeight: '400',
-    textAlign: 'center',
-    marginBottom: '30px',
-    lineHeight: '2rem'
-  },
-
-  slickImage: {
-    display: 'inline-block',
-    width: '70%',
-    marginTop: '55px',
-    marginLeft: 'auto',
-    marginRight: 'auto'
-  },
-
-  form: {
-    maxWidth: '500px',
-    marginLeft: 'auto',
-    marginRight: 'auto'
-  },
-
-  summoner: {
-    marginTop: '75px'
-  },
-
-  btnPrev: {
-    float: 'left',
-    outline: 'none',
-    color: '#52bdab',
-    border: 'none',
-    borderRadius: '5px',
-    padding: '10px 25px',
-    fontSize: '1rem',
-    height: '45px',
-    marginTop: '30px',
-    fontWeight: '500',
-    cursor: 'pointer',
-    background: 'transparent'
-  },
-
-  btnNext: {
-    float: 'right',
-    outline: 'none',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '5px',
-    padding: '10px 25px',
-    fontSize: '1rem',
-    height: '45px',
-    marginTop: '30px',
-    fontWeight: '500',
-    cursor: 'pointer',
-    background: 'linear-gradient(to right, #52bdab 0%,#6BB6D6 100%)'
-  },
-
-  codeCase: {
-    border: '1px solid #ccc',
-    padding: '20px',
-    borderRadius: '4px',
-    width: '300px',
-    position: 'relative',
-    margin: '25px auto'
-  },
-
-  code: {
-    fontSize: '1.5rem',
-    color: '#333',
-    lineHeight: '1',
-    verticalAlign: 'middle'
-  },
-
-  codeBtn: {
-    height: '64px',
-    backgroundColor: 'transparent',
-    border: 'none',
-    top: '0',
-    position: 'absolute',
-    right: '0',
-    width: '125px',
-    fontSize: '1rem',
-    borderLeft: '1px solid #ccc',
-    outline: 'none',
-    color: '#333',
-    lineHeight: '64px',
-    textAlign: 'center',
-    cursor: 'pointer',
-    transition: '.15s',
-
-    '&:hover': {
-      backgroundColor: '#f9f9f9'
-    }
-  },
-
-  mastery: {
-    maxWidth: '90%',
-    borderRadius: '6px'
-  },
-
-  stepImage: {
-    flexBasis: '70%',
-
-    '@media (max-width: 750px)': {
-      flexBasis: '100%'
-    }
-  },
-
-  steps: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    flexWrap: 'wrap'
-  },
-
-  stepList: {
-    marginTop: '50px',
-    flexBasis: '30%',
-
-    '@media (max-width: 750px)': {
-      flexBasis: '100%'
-    }
-  },
-
-  step: {
-    marginBottom: '30px'
-  },
-
-  stepNumber: {
-    display: 'block',
-    fontSize: '1.5rem',
-    color: '#333',
-    marginBottom: '10px'
-  },
-
-  stepText: {
-    color: '#999'
-  }
-}
-
 class Onboard extends Component {
   constructor () {
     super()
@@ -335,94 +161,252 @@ class Onboard extends Component {
     return (
       <Slider ref={c => this.slider = c } {...settings}>
         <div>
-          <img className={style(styles.slickImage)} src="static/placeholder.svg" alt=""/>
+          <img className="slickImage" src="static/placeholder.svg" alt=""/>
 
-          <h1 className={style(styles.title)}>Bem vindo ao Ritoplz</h1>
-          <h2 className={style(styles.subtitle)}>Vamos ajudar você a configurar sua conta</h2>
+          <h1 className="title">Bem vindo ao Ritoplz</h1>
+          <h2 className="subtitle">Vamos ajudar você a configurar sua conta</h2>
 
-          <button className={style(styles.btnNext)} onClick={this.nextSlide}>Próximo</button>
+          <button className="btnNext" onClick={this.nextSlide}>Próximo</button>
         </div>
 
         <div>
-          <h1 className={style(styles.title)}>Escolha sua localização</h1>
-          <h2 className={style(styles.subtitle)}>Essa será sua localização no Rankings</h2>
+          <h1 className="title">Escolha sua localização</h1>
+          <h2 className="subtitle">Essa será sua localização no Rankings</h2>
 
-          <div className={style(styles.form)}>
-            <fieldset className={style(styles.formInput)}>
-              <label className={style(styles.label)}>País</label>
+          <div className="form">
+            <fieldset className="formInput">
+              <label className="label">País</label>
               <Select options={this.state.countryList} value={this.state.country} onChange={this.handleCountry}/>
             </fieldset>
 
-            <fieldset className={style(styles.formInput)}>
-              <label className={style(styles.label)}>Estado</label>
+            <fieldset className="formInput">
+              <label className="label">Estado</label>
               <Select options={this.state.stateList} value={this.state.stateParam} onChange={this.handleState}/>
             </fieldset>
 
-            <fieldset className={style(styles.formInput)}>
-              <label className={style(styles.label)}>Cidade</label>
+            <fieldset className="formInput">
+              <label className="label">Cidade</label>
               <Select options={this.state.cityList} value={this.state.city} onChange={this.handleCity}/>
             </fieldset>
           </div>
 
-          <button className={style(styles.btnPrev)} onClick={this.previousSlide}>Anterior</button>
-          <button className={style(styles.btnNext)} onClick={this.submitLocation} disabled={this.state.requesting}>Próximo</button>
+          <button className="btnPrev" onClick={this.previousSlide}>Anterior</button>
+          <button className="btnNext" onClick={this.submitLocation} disabled={this.state.requesting}>Próximo</button>
         </div>
 
         <div>
-          <div className={style(styles.summoner)}>
-            <h1 className={style(styles.title)}>Vamos adicionar seu invocador</h1>
-            <h2 className={style(styles.subtitle)}>Nome do seu invocador</h2>
+          <div className="summoner">
+            <h1 className="title">Vamos adicionar seu invocador</h1>
+            <h2 className="subtitle">Nome do seu invocador</h2>
 
-            <div className={style(styles.form)}>
-              <fieldset className={style(styles.formInput)}>
-                <label className={style(styles.label)}>Nome do Invocador</label>
-                <input className={style(styles.input)} type="text" ref={node => this.summoner = node}/>
+            <div className="form">
+              <fieldset className="formInput">
+                <label className="label">Nome do Invocador</label>
+                <input className="input" type="text" ref={node => this.summoner = node}/>
               </fieldset>
             </div>
           </div>
 
-          <button className={style(styles.btnPrev)} onClick={this.previousSlide}>Anterior</button>
-          <button className={style(styles.btnNext)} onClick={this.submitSummoner} disabled={this.state.requesting}>Próximo</button>
+          <button className="btnPrev" onClick={this.previousSlide}>Anterior</button>
+          <button className="btnNext" onClick={this.submitSummoner} disabled={this.state.requesting}>Próximo</button>
         </div>
 
         <div>
-          <h1 className={style(styles.title)}>Confirme seu invocador</h1>
-          <h2 className={style(styles.subtitle)}>Siga as instruções abaixo para confirmar seu invocador</h2>
+          <h1 className="title">Confirme seu invocador</h1>
+          <h2 className="subtitle">Siga as instruções abaixo para confirmar seu invocador</h2>
 
-          <div className={style(styles.steps)}>
-            <div className={style(styles.stepImage)}>
-              <img className={style(styles.mastery)} src="static/mastery-lol.png" alt=""/>
+          <div className="steps">
+            <div className="stepImage">
+              <img className="mastery" src="static/mastery-lol.png" alt=""/>
             </div>
 
-            <ul className={style(styles.stepList)}>
-              <li className={style(styles.step)}>
-                <span className={style(styles.stepNumber)}>Passo 1.</span>
-                <span className={style(styles.stepText)}>Crie ou edite uma Página de Talentos ou Página de Runas</span>
+            <ul className="stepList">
+              <li className="step">
+                <span className="stepNumber">Passo 1.</span>
+                <span className="stepText">Crie ou edite uma Página de Talentos ou Página de Runas</span>
               </li>
 
-              <li className={style(styles.step)}>
-                <span className={style(styles.stepNumber)}>Passo 2.</span>
-                <span className={style(styles.stepText)}>Use esse código ("{this.state.code}") como nome da página</span>
+              <li className="step">
+                <span className="stepNumber">Passo 2.</span>
+                <span className="stepText">Use esse código ("{this.state.code}") como nome da página</span>
               </li>
 
-              <li className={style(styles.step)}>
-                <span className={style(styles.stepNumber)}>Passo 3.</span>
-                <span className={style(styles.stepText)}>Clique em confirmar invocador abaixo</span>
+              <li className="step">
+                <span className="stepNumber">Passo 3.</span>
+                <span className="stepText">Clique em confirmar invocador abaixo</span>
               </li>
             </ul>
           </div>
 
-          <div className={style(styles.codeCase)}>
-            <p className={style(styles.code)}>{this.state.code}</p>
+          <div className="codeCase">
+            <p className="code">{this.state.code}</p>
 
             <CopyToClipboard text={this.state.code} onCopy={this.copy}>
-              <span className={style(styles.codeBtn)}>Copiar código</span>
+              <span className="codeBtn">Copiar código</span>
             </CopyToClipboard>
           </div>
 
-          <button className={style(styles.btnPrev)} onClick={this.previousSlide}>Anterior</button>
-          <button className={style(styles.btnNext)} onClick={this.confirmSummoner} disabled={this.state.requesting}>Confirmar Invocador</button>
+          <button className="btnPrev" onClick={this.previousSlide}>Anterior</button>
+          <button className="btnNext" onClick={this.confirmSummoner} disabled={this.state.requesting}>Confirmar Invocador</button>
         </div>
+
+        <style jsx>{`
+          .formInput {
+            border: none;
+            marginBottom: 20px;
+          }
+
+          .label {
+            display: inline-block;
+            marginBottom: 10px;
+            fontWeight: 600,
+            fontSize: 1.15rem;
+            color: #333;
+          }
+
+          .input {
+            padding: 20px 15px;
+            width: 100%;
+            border: 1px solid #eee;
+            borderRadius: 5px;
+            fontSize: 1.1rem;
+            outline: none;
+          }
+
+          .title {
+            textAlign: center;
+            color: #333;
+            fontSize: 2rem;
+            fontWeight: 500;
+            marginBottom: 10px;
+          }
+
+          .subtitle {
+            color: #999;
+            fontSize: 1.15rem;
+            fontWeight: 400;
+            textAlign: center;
+            marginBottom: 30px;
+            lineHeight: 2rem;
+          }
+
+          .slickImage {
+            display: inline-block;
+            width: 70%;
+            marginTop: 55px;
+            marginLeft: auto;
+            marginRight: auto;
+          }
+
+          .form {
+            maxWidth: 500px;
+            marginLeft: auto;
+            marginRight: auto;
+          }
+
+          .summoner {
+            marginTop: 75px;
+          }
+
+          .btnPrev {
+            float: left;
+            outline: none;
+            color: #52bdab;
+            border: none;
+            borderRadius: 5px;
+            padding: 10px 25px;
+            fontSize: 1rem;
+            height: 45px;
+            marginTop: 30px;
+            fontWeight: 500;
+            cursor: pointer;
+            background: transparent;
+          }
+
+          .btnNext {
+            float: right;
+            outline: none;
+            color: #fff;
+            border: none;
+            borderRadius: 5px;
+            padding: 10px 25px;
+            fontSize: 1rem;
+            height: 45px;
+            marginTop: 30px;
+            fontWeight: 500;
+            cursor: pointer;
+            background: linear-gradient(to right, #52bdab 0%,#6BB6D6 100%);
+          }
+
+          .codeCase {
+            border: 1px solid #ccc;
+            padding: 20px;
+            borderRadius: 4px;
+            width: 300px;
+            position: relative;
+            margin: 25px auto;
+          }
+
+          .code {
+            fontSize: 1.5rem;
+            color: #333;
+            lineHeight: 1;
+            verticalAlign: middle;
+          }
+
+          .codeBtn {
+            height: 64px;
+            backgroundColor: transparent;
+            border: none;
+            top: 0;
+            position: absolute;
+            right: 0;
+            width: 125px;
+            fontSize: 1rem;
+            borderLeft: 1px solid #ccc;
+            outline: none;
+            color: #333;
+            lineHeight: 64px;
+            textAlign: center;
+            cursor: pointer;
+            transition: .15s;
+          }
+
+          .mastery {
+            maxWidth: 90%;
+            borderRadius: 6px;
+          }
+
+          .stepImage {
+            flexBasis: 70%;
+          }
+
+          .steps {
+            display: flex;
+            justifyContent: space-between;
+            flexWrap: wrap;
+          }
+
+          .stepList {
+            marginTop: 50px;
+            flexBasis: 30%;
+          }
+
+          .step {
+            marginBottom: 30px;
+          }
+
+          .stepNumber {
+            display: block;
+            fontSize: 1.5rem;
+            color: #333;
+            marginBottom: 10px;
+          }
+
+          .stepText {
+            color: #999;
+          }
+        `}</style>
       </Slider>
     )
   }
