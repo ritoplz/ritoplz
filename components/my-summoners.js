ebb2163d3c93127cb62b898cc2c24ffffb428fbb
Bu Kinoshita <bukinoshita@gmail.com>
2017-05-01 16:44:06 -0400

    Upgrade my-summoners

--- components/my-summoners.js
+++ components/my-summoners.js
@@ -1,47 +1,10 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { style } from 'next/css'
 import summonerCover from 'ritoplz-summoner'
 
 import Summoner from './summoner'
 import ModalAddSummoner from './../containers/modal-add-summoner'
-
-const styles = {
-  header: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-
-  title: {
-    color: '#333',
-    marginBottom: '50px',
-    fontWeight: 400
-  },
-
-  btn: {
-    color: '#fff',
-    border: 'none',
-    borderRadius: '5px',
-    padding: '10px 25px',
-    fontSize: '.9rem',
-    height: '50px',
-    marginTop: '-50px',
-    fontWeight: '500',
-    cursor: 'pointer',
-    background: 'linear-gradient(to right, #52bdab 0%,#6BB6D6 100%)'
-  },
-
-  row: {
-    marginLeft: '-15px',
-    marginRight: '-15px',
-    display: 'flex',
-    flexFlow: 'row wrap',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  }
-}
 
 class MySummoners extends Component {
   constructor () {
@@ -71,12 +34,12 @@ class MySummoners extends Component {
   render () {
     return (
       <section>
-        <header className={style(styles.header)}>
-          <h2 className={style(styles.title)}>Meus Invocadores</h2>
-          <button className={style(styles.btn)} onClick={this.handleModal}>Adicionar Invocador</button>
+        <header className="header">
+          <h2 className="title">Meus Invocadores</h2>
+          <button className="btn" onClick={this.handleModal}>Adicionar Invocador</button>
         </header>
 
-        <div className={style(styles.row)}>
+        <div className="row">
           {this.props.summoners.map((summoner, i) => {
             const cover = summonerCover()
 
@@ -85,6 +48,42 @@ class MySummoners extends Component {
         </div>
 
         <ModalAddSummoner open={this.state.modalAddSummoner}/>
+
+        <style jsx>{`
+          .header {
+            display: flex;
+            justifyContent: space-between;
+            alignItems: center;
+          }
+
+          .title {
+            color: #333;
+            marginBottom: 50px;
+            fontWeight: 400
+          }
+
+          .btn {
+            color: #fff;
+            border: none;
+            borderRadius: 5px;
+            padding: 10px 25px;
+            fontSize: .9rem;
+            height: 50px;
+            marginTop: -50px;
+            fontWeight: 500;
+            cursor: pointer;
+            background: linear-gradient(to right, #52bdab 0%,#6BB6D6 100%);
+          }
+
+          .row {
+            marginLeft: -15px;
+            marginRight: -15px;
+            display: flex;
+            flexFlow: row wrap;
+            alignItems: center;
+            justifyContent: space-between;
+          }
+        `}</style>
       </section>
     )
   }
