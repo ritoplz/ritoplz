237b6096718d1e11d8d66dccba336246cc3929c7
Bu Kinoshita <bukinoshita@gmail.com>
2017-05-02 18:23:05 -0400

    Fix configure store

--- store/configure-store.js
+++ store/configure-store.js
@@ -1,5 +1,7 @@
 'use strict'
 
+/* global window */
+
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
@@ -7,16 +9,18 @@ import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
 
 const logger = createLogger()
+const middlewares = [logger, thunk]
 
 const configureStore = initialState => {
   return createStore(
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(thunk),
-      typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
-      ? window.devToolsExtension()
-      : f => f
+      applyMiddleware(...middlewares),
+      typeof window === 'object' &&
+        typeof window.devToolsExtension !== 'undefined'
+        ? window.devToolsExtension()
+        : f => f
     )
   )
 }
