5595d3c8d3d93b52534b038bfdb1e7bbff096d5c
Bu Kinoshita <bukinoshita@gmail.com>
2017-05-02 11:57:22 -0400

    Major update

---
+++ components/ui/badge.js
@@ -0,0 +1,36 @@
+'use strict'
+
+import React from 'react'
+import { colors } from './theme'
+
+const Badge = ({children, type = 'primary'}) => (
+  <div className={type}>
+    {children}
+
+    <style jsx>{`
+      div {
+        border-radius: 3px;
+        padding: 4px 6px;
+        font-size: .8rem;
+        display: inline-block;
+        vertical-align: middle;
+        margin-left: 5px;
+        color: ${colors.white};
+      }
+
+      .primary {
+        background-color: ${colors.primary};
+      }
+
+      .success {
+        background-color: ${colors.success};
+      }
+
+      .danger {
+        background-color: ${colors.danger};
+      }
+    `}</style>
+  </div>
+)
+
+export default Badge
