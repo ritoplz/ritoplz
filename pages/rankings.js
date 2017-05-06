377cdfbf76df27fea728b233c8ef029850166ce9
Bu Kinoshita <bukinoshita@gmail.com>
2017-05-06 14:08:21 -0400

    Add infinite loading

--- pages/rankings.js
+++ pages/rankings.js
@@ -3,6 +3,7 @@
 import { Component } from 'react'
 import withRedux from 'next-redux-wrapper'
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroller'
 
 import store from './../store/configure-store'
 import Page from './../layouts/page'
@@ -12,29 +13,69 @@ import Header from './../components/header'
 import fetchRankings from './../actions/fetch-rankings'
 
 class Rankings extends Component {
+  constructor() {
+    super()
+
+    this.loadItems = this.loadItems.bind(this)
+
+    this.state = {
+      nextPage: false
+    }
+  }
+
   componentDidMount() {
-    this.props.fetchRankings()
+    this.props.fetchRankings({ country: 'BR' }).then(({ data }) => {
+      this.setState({
+        nextPage: data.next_page,
+        skip: 0,
+        summoners: data.summoners
+      })
+    })
+  }
+
+  loadItems() {
+    this.setState({ nextPage: false })
+
+    this.props
+      .fetchRankings({ skip: this.state.skip + 100, country: 'BR' })
+      .then(({ data }) => {
+        const summoners = this.state.summoners.concat(data.summoners)
+        this.setState({
+          nextPage: data.next_page,
+          skip: this.state.skip + 100,
+          summoners
+        })
+      })
   }
 
   render() {
     let rankings
 
-    if (this.props.summoners) {
-      rankings = this.props.summoners.map((user, index) => (
-        <RankingUser user={user} key={user._id} position={index + 1} />
-      ))
+    if (this.state.summoners) {
+      rankings = this.state.summoners.map((user, index) => {
+        return <RankingUser user={user} key={user._id} position={index + 1} />
+      })
     } else {
-      rankings = <h1>Loading...</h1>
+      rankings = <div className="loader">Loading ...</div>
     }
+
+    const loader = <div className="loader">Loading ...</div>
 
     return (
       <Page>
         <Header />
 
         <Row>
-          <div className="rankings">
-            {rankings}
-          </div>
+          <InfiniteScroll
+            pageStart={0}
+            loadMore={this.loadItems}
+            hasMore={this.state.nextPage}
+            loader={loader}
+          >
+            <div className="rankings">
+              {rankings}
+            </div>
+          </InfiniteScroll>
         </Row>
 
         <style jsx>{`
@@ -61,7 +102,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    fetchRankings: () => dispatch(fetchRankings())
+    fetchRankings: params => dispatch(fetchRankings(params))
   }
 }
 
