5dbcb7965065422c03a99bc54d1864d2a9383212
Bu Kinoshita <bukinoshita@gmail.com>
2017-05-05 21:56:37 -0400

    Add fetch rankings

--- pages/rankings.js
+++ pages/rankings.js
@@ -1,111 +1,68 @@
 'use strict'
 
+import { Component } from 'react'
+import withRedux from 'next-redux-wrapper'
+import PropTypes from 'prop-types'
+
+import store from './../store/configure-store'
 import Page from './../layouts/page'
 import { Row } from './../components/ui'
 import RankingUser from './../components/ranking-user'
 import Header from './../components/header'
+import fetchRankings from './../actions/fetch-rankings'
 
-const rankingList = [
-  {
-    name: 'Pedro Álvares',
-    summonerLevel: 30,
-    profileIconId: 28,
-    region: 'br',
-    recentMatches: {
-      lastPlayedSolo: {
-        win: true,
-        date: '16 hours ago'
-      }
-    },
-    city: 'Goiânia',
-    country: 'BR',
-    state: 'Goiás',
-    username: 'Matheus Nery',
-    rankedSolo: {
-      elo: 7943,
-      isHotStreak: false,
-      losses: 148,
-      wins: 171,
-      lp: 73,
-      division: 'I',
-      tier: 'CHALLENGER'
-    }
-  },
-  {
-    name: 'hídden',
-    country: 'BR',
-    state: 'São Paulo',
-    city: 'São Paulo',
-    username: 'Carlos Eduardo',
-    summonerLevel: 30,
-    profileIconId: 505,
-    region: 'br',
-    recentMatches: {
-      lastPlayedSolo: {
-        win: true,
-        date: '3 days ago'
-      }
-    },
-    rankedSolo: {
-      elo: 6852,
-      isHotStreak: false,
-      losses: 216,
-      wins: 246,
-      lp: 302,
-      division: 'I',
-      tier: 'MASTER'
-    }
-  },
-  {
-    name: 'NL Marcelinho',
-    country: 'BR',
-    state: 'Pernambuco',
-    city: 'Recife',
-    username: 'Marcelo ',
-    summonerLevel: 30,
-    profileIconId: 1598,
-    region: 'br',
-    recentMatches: {
-      lastPlayedFlex: {
-        win: true,
-        date: '2 days ago'
-      },
-      lastPlayedSolo: {
-        win: false,
-        date: '2 days ago'
-      }
-    },
-    rankedSolo: {
-      elo: 6674,
-      isHotStreak: false,
-      losses: 126,
-      wins: 151,
-      lp: 124,
-      division: 'I',
-      tier: 'MASTER'
-    }
+class Rankings extends Component {
+  componentDidMount() {
+    this.props.fetchRankings()
   }
-]
 
-const Rankings = () => {
-  return (
-    <Page>
-      <Header />
+  render() {
+    let rankings
 
-      <Row>
-        <div className="rankings">
-          {rankingList.map(user => <RankingUser user={user} />)}
-        </div>
-      </Row>
+    if (this.props.summoners) {
+      rankings = this.props.summoners.map((user, index) => (
+        <RankingUser user={user} key={user._id} position={index + 1} />
+      ))
+    } else {
+      rankings = <h1>Loading...</h1>
+    }
 
-      <style jsx>{`
-        .rankings {
-          padding-top: 50px;
-          padding-bottom: 50px;
-        }
-      `}</style>
-    </Page>
-  )
+    return (
+      <Page>
+        <Header />
+
+        <Row>
+          <div className="rankings">
+            {rankings}
+          </div>
+        </Row>
+
+        <style jsx>{`
+          .rankings {
+            padding-top: 50px;
+            padding-bottom: 50px;
+          }
+        `}</style>
+      </Page>
+    )
+  }
 }
 
-export default Rankings
+Rankings.propTypes = {
+  fetchRankings: PropTypes.func.isRequired,
+  summoners: PropTypes.array
+}
+
+const mapStateToProps = state => {
+  return {
+    summoners: state.rankings.data.summoners
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchRankings: () => dispatch(fetchRankings())
+  }
+}
+
+export default withRedux(store, mapStateToProps, mapDispatchToProps)(Rankings)
