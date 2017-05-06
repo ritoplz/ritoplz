f80539ad775b84648639791b97707d14c9c55141
Bu Kinoshita <bukinoshita@gmail.com>
2017-05-06 14:29:59 -0400

    Add hot streak

--- services/api.js
+++ services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import { getToken, isLogged } from './auth'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://api.ritoplz.com',
   headers: {
     Accept: 'application/json'
   }
