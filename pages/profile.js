6c7b0aab898a2600f9392902dea443d9d1df0271
Bu Kinoshita <bukinoshita@gmail.com>
2017-05-08 20:48:38 -0400

    Add profile page

---
+++ pages/profile.js
@@ -0,0 +1,52 @@
+'use strict'
+
+import { Component } from 'react'
+import withRedux from 'next-redux-wrapper'
+import PropTypes from 'prop-types'
+
+import fetchAccount from './../actions/fetch-account'
+import Page from './../layouts/page'
+import Header from './../components/header'
+import { Row } from './../components/ui'
+import store from './../store/configure-store'
+
+class Profile extends Component {
+  componentDidMount() {
+    this.props.fetchAccount()
+  }
+
+  render() {
+    return (
+      <Page>
+        <Header />
+        <Row>
+          <h1>Profile</h1>
+
+          {this.props.user._id}
+          {this.props.user.email}
+          {this.props.user.emailConfirmed}
+          {this.props.user.name}
+        </Row>
+      </Page>
+    )
+  }
+}
+
+Profile.propTypes = {
+  user: PropTypes.object,
+  fetchAccount: PropTypes.func
+}
+
+const mapStateToProps = state => {
+  return {
+    user: state.account.data.user
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchAccount: () => dispatch(fetchAccount())
+  }
+}
+
+export default withRedux(store, mapStateToProps, mapDispatchToProps)(Profile)
