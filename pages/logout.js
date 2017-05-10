585bd0c089d3e9b47462dc586e0bb59359b9aba3
Bu Kinoshita <bukinoshita@gmail.com>
2017-05-10 19:26:17 -0400

    Add logout

---
+++ pages/logout.js
@@ -0,0 +1,47 @@
+'use strict'
+
+import { Component } from 'react'
+import withRedux from 'next-redux-wrapper'
+import Router from 'next/router'
+
+import Logo from './../components/logo'
+import Header from './../components/header'
+import Page from './../layouts/page'
+import { colors, typography } from './../components/ui/theme'
+import store from './../store/configure-store'
+import { logout } from './../services/auth'
+
+class Logout extends Component {
+  componentDidMount() {
+    logout()
+    Router.push('/')
+  }
+
+  render() {
+    return (
+      <Page>
+        <Header />
+        <div>
+          <Logo size="200px" />
+          <h2>😢</h2>
+        </div>
+
+        <style jsx>{`
+          div {
+            padding-top: 150px;
+            text-align: center;
+          }
+
+          h2 {
+            margin-top: 20px;
+            font-size: ${typography.f30};
+            font-weight: 400;
+            color: ${colors.heading};
+          }
+        `}</style>
+      </Page>
+    )
+  }
+}
+
+export default withRedux(store, null, null)(Logout)
